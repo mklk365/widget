0e032e2dfe6ce5f6b29aac5089508853a2d3e441
mak <mklk.rrgpro@mail.ru>
2025-06-09 12:36:47 +0500

    Import my funk in widget

--- src/widget.py
+++ src/widget.py
@@ -1,3 +1,6 @@
+from .masks import get_mask_account, get_mask_card_number
+
+
 def mask_account_card(card_or_schet_number: str) -> str:
     """Функция обрабатывает информацию о картах и счетах"""
     digit_number = ""
