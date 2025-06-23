e827d85de86e4e6b56438e1d80e034fd703fd2bd
mak <mklk.rrgpro@mail.ru>
2025-06-23 16:44:33 +0500

    test_mask.py is ready and fixed masks.py

--- src/masks.py
+++ src/masks.py
@@ -1,10 +1,12 @@
+from typing import Any
+
 def get_mask_card_number(card_number: Any) -> str:
     """Функция принимает номер карты и возвращает ее маску"""
     str_number = str(card_number)
-    return f"{str_number[0:4]} {str_number[4:6]} **** {str_number[12:16]}"
+    return f"{str_number[0:4]} {str_number[4:6]}** **** {str_number[12:16]}"
 
 
 def get_mask_account(schet_number: Any) -> str:
     """Функция принимает номер счета и возвращает его маску"""
     str_number = str(schet_number)
-    return f"*{str_number[-4:]}"
+    return f"**{str_number[-4:]}"
