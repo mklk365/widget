4c2e9e76ae256421cc1c85352d2f84426de5b0b3
mak <mklk.rrgpro@mail.ru>
2025-07-17 13:46:09 +0500

    fix err in decorators, test_decorators

--- src/decorators.py
+++ src/decorators.py
@@ -1,6 +1,6 @@
 from datetime import datetime
 from functools import wraps
-import os
+
 
 
 def log(filename=None):
@@ -13,7 +13,7 @@ def log(filename=None):
         @wraps(func)
         def wrapper(*args, **kwargs):
             time_start = datetime.now()
-            start_msg = f"Name:  {func.__name__}\nStart: {time_start}"
+            start_msg = f"Start: {time_start}"
             print(start_msg)
             if filename:
                 with open(filename, 'a') as file:
@@ -21,7 +21,7 @@ def log(filename=None):
             try:
                 result = func(*args, **kwargs)  # Вызов исходной функции
                 time_end = datetime.now()
-                result_msg = f"End:   {time_end}\nResult: {result}"
+                result_msg = f"Name: {func.__name__} ok\nResult: {result}\nEnd: {time_end}"
                 print(result_msg)
                 if filename:
                     with open(filename, 'a') as file:
@@ -29,7 +29,7 @@ def log(filename=None):
                 return result
             except Exception as e:
                 time_end = datetime.now()
-                err_msg = f"End:   {time_end}\nError: {e}"
+                err_msg = f"End: {time_end}\nError: {e}\nInputs:\nArgs: {args}\nKwargs: {kwargs}"
                 print(err_msg)
                 if filename:
                     with open(filename, 'a') as file:
--- tests/test_decorators.py
+++ tests/test_decorators.py
@@ -1,11 +1,27 @@
 import pytest
+from datetime import datetime
+
 from decorators import log
 
 
-def test_log():
-    @log
-    def get_mask_account(num_acc):
-        return num_acc
+@log()
+def successful_func():
+    return "Success!"
 
-    result = get_mask_account('73654108430135874305')
-    assert result == '**4305'
+@log()
+def failing_func():
+    raise ValueError("Test error")
+
+def test_start_message_is_printed(capsys):
+    """Проверяем, что в stdout выводится 'Start:'"""
+    successful_func()
+    captured = capsys.readouterr()
+    assert "Start:" in captured.out
+
+def test_error_message_is_printed_on_exception(capsys):
+    """Проверяем, что при ошибке выводится сообщение с её текстом"""
+    with pytest.raises(ValueError):
+        failing_func()
+    captured = capsys.readouterr()
+    assert "Error:" in captured.out
+    assert "Test error" in captured.out
